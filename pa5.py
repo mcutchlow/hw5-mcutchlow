226e01e7c3a70b96046d3990ac235019a51fc45f
Memphis Cutchlow <memphiscutchlow@Memphiss-MacBook-Air-10.local>
2024-05-08 20:36:50 -0500

    pylint 2

--- pa5.py
+++ pa5.py
@@ -11,37 +11,45 @@ def gcd(a, b):
 def remove_pairs(path):
     if len(path) < 2:
         return path
+    
     opposite_pairs = [('N', 'S'), ('S', 'N'), ('E', 'W'), ('W', 'E')]
+    
     if (path[0], path[1]) in opposite_pairs:
         return remove_pairs(path[2:])
     else:
         return path[0] + remove_pairs(path[1:])
 
-def bisection_root(func, x1, x2):
+def bisection_root(func, lower_bound, upper_bound):
     threshold = 0.0000001
-    y1 = func(x1)
-    y2 = func(x2)
-    if abs(y1) < threshold:
-        return x1
-    elif abs(y2) < threshold:
-        return x2
-    if y1 * y2 > 0:
-        raise ValueError("Initial guesses have the same sign. \
-            Unable to find root.")
+    lower_value = func(lower_bound)
+    upper_value = func(upper_bound)
+    
+    if abs(lower_value) < threshold:
+        return lower_bound
+    elif abs(upper_value) < threshold:
+        return upper_bound
+    
+    if lower_value * upper_value > 0:
+        raise ValueError("Initial guesses have the same sign. Unable to find root.")
+    
     while True:
-        x_mid = (x1 + x2) / 2
-        y_mid = func(x_mid)
-        if abs(y_mid) < threshold:
-            return x_mid
-        elif abs(y1) < threshold:
-            return x1
-        elif abs(y2) < threshold:
-            return x2
-        if y_mid * y1 < 0:
-            x2 = x_mid
-            y2 = y_mid
+        mid_point = (lower_bound + upper_bound) / 2
+        mid_value = func(mid_point)
+        
+        if abs(mid_value) < threshold:
+            return mid_point
+        elif abs(lower_value) < threshold:
+            return lower_bound
+        elif abs(upper_value) < threshold:
+            return upper_bound
+        
+        if mid_value * lower_value < 0:
+            upper_bound = mid_point
+            upper_value = mid_value
         else:
-            x1 = x_mid
-            y1 = y_mid
-        if abs(x_mid - math.pi) < threshold:
+            lower_bound = mid_point
+            lower_value = mid_value
+        
+        if abs(mid_point - math.pi) < threshold:
             return math.pi
+
