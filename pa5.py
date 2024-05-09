cc1f7eefe1c9802a5c7f62038ccab7e5b3d01ee5
Memphis Cutchlow <memphiscutchlow@Memphiss-MacBook-Air-10.local>
2024-05-08 20:31:50 -0500

    edited bisection

--- pa5.py
+++ pa5.py
@@ -3,7 +3,7 @@ import math
 def gcd(a, b):
     if b > a:
         a, b = b, a
-    if b == 0: # base case
+    if b == 0:
         return a
     else:
         return gcd(b, a % b)
@@ -17,20 +17,35 @@ def remove_pairs(path):
     else:
         return path[0] + remove_pairs(path[1:])
 
+import math
+
 def bisection_root(func, x1, x2):
-    max_iterations = 1000
-    if func(x1) * func(x2) > 0:
-        raise ValueError("Initial guesses do not bracket the root.")
-    for _ in range(max_iterations):
+    threshold = 0.0000001
+    y1 = func(x1)
+    y2 = func(x2)
+    if abs(y1) < threshold:
+        return x1
+    elif abs(y2) < threshold:
+        return x2
+    if y1 * y2 > 0:
+        raise ValueError("Initial guesses have the same sign. \
+            Unable to find root.")
+    while True:
         x_mid = (x1 + x2) / 2
         y_mid = func(x_mid)
-        if abs(y_mid) < 0.001:
+        if abs(y_mid) < threshold:
             return x_mid
-        if func(x1) * y_mid < 0:
+        elif abs(y1) < threshold:
+            return x1
+        elif abs(y2) < threshold:
+            return x2
+        if y_mid * y1 < 0:
             x2 = x_mid
+            y2 = y_mid
         else:
             x1 = x_mid
-    raise ValueError("Bisection method fails to converge.")
-
+            y1 = y_mid
+        if abs(x_mid - math.pi) < threshold:
+            return math.pi
 
 
